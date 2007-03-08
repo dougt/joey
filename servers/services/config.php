99ab0f10ef657fc885e69f21dbd545ddcf2f0a58
dougt@meer.net <dougt@meer.net@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-03-08 23:25:04 +0000

    removing the xmlrpc service in favor of rest
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@2489 4eb1ac78-321c-0410-a911-ec516a8615a5

--- servers/services/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$dbname="c_joey";
-$host="localhost";
-$user="root";
-$dbh=mysql_connect ($host,$user,"password") or die ('I cannot connect to the database because: ' . mysql_error(). '');
-mysql_select_db ("$dbname") or die('I cannot select the database because: ' . mysql_error());
-?>
