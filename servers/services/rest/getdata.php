5a5d1f3ff7c28b614d2599dad9e69e3b637efabc
dougt@meer.net <dougt@meer.net@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-03-11 04:45:48 +0000

    Moving FileOps into a shared location
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@2523 4eb1ac78-321c-0410-a911-ec516a8615a5

--- servers/services/rest/getdata.php
+++ servers/services/rest/getdata.php
@@ -1,7 +1,6 @@
 <?php
-    function __autoload($class_name) {
-     require_once $class_name . '.class.php';
-    }
+
+    require_once('../../libraries/FileOps.class.php');
 
     session_start();
 
--- servers/services/rest/upload.php
+++ servers/services/rest/upload.php
@@ -1,8 +1,5 @@
 <?php
-
-    function __autoload($class_name) {
-     require_once $class_name . '.class.php';
-    }
+    require_once('../../libraries/FileOps.class.php');
     
     session_start();
     
