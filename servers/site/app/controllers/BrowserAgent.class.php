021754a0305d504523eaf84d58e86ae879fb6961
michael@michaelyuan.com <michael@michaelyuan.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-03-12 07:08:51 +0000

    fix for other mobile devices
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@2556 4eb1ac78-321c-0410-a911-ec516a8615a5

--- servers/site/app/controllers/BrowserAgent.class.php
+++ servers/site/app/controllers/BrowserAgent.class.php
@@ -3,7 +3,11 @@ class BrowserAgent {
 
   function isMobile () {
     $agent=$_SERVER["HTTP_USER_AGENT"];
-    return preg_match('/nokia/i', $agent);
+
+    // If mobile phone, see http://www.developershome.com/wap/detection/detection.asp?page=userAgentHeader
+    $res = preg_match('/nokia|motorola|mot\-|samsung|sec\-|lg\-|sonyericsson|sie\-|up\.b|up\//i', $agent);
+
+    return $res;
   }
 
 }
