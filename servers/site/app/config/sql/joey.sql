4005b2ecd077a0428b57817bbfd7173fb92d4ea6
wclouser@mozilla.com <wclouser@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-03-14 21:29:07 +0000

    SQL tweaks
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@2642 4eb1ac78-321c-0410-a911-ec516a8615a5

--- servers/site/app/config/sql/joey.sql
+++ servers/site/app/config/sql/joey.sql
@@ -1,5 +1,19 @@
--- Mozilla Labs: Joey SQL Dump
+-- MySQL dump 10.10
 --
+-- Host: localhost    Database: c_joey
+-- ------------------------------------------------------
+-- Server version	5.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `cake_sessions`
@@ -65,6 +79,33 @@ CREATE TABLE `files` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Table structure for table `operators`
+--
+
+DROP TABLE IF EXISTS `operators`;
+CREATE TABLE `operators` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `provider` varchar(255) NOT NULL default '',
+  `emaildomain` varchar(255) NOT NULL default '',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `phones`
+--
+
+DROP TABLE IF EXISTS `phones`;
+CREATE TABLE `phones` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL default '',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
 -- Table structure for table `uploads`
 --
 
@@ -92,12 +133,27 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL default '',
   `email` varchar(255) NOT NULL default '',
   `confirmationcode` varchar(255) default NULL,
-  `phone` varchar(255) NOT NULL default '',
-  `phoneoperatorid` varchar(255) NOT NULL default '',
+  `phone_id` int(11) unsigned NOT NULL default '0',
+  `operator_id` int(11) unsigned NOT NULL default '0',
+  `phonenumber` varchar(255) NOT NULL default '',
   `notes` text,
   `disabled` int(1) NOT NULL default '0',
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY  (`id`),
-  UNIQUE KEY `username` (`username`)
+  UNIQUE KEY `username` (`username`),
+  KEY `users_ibfk_1` (`phone_id`),
+  KEY `users_ibfk_2` (`operator_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`phone_id`) REFERENCES `phones` (`id`),
+  CONSTRAINT `users_ibfk_2` FOREIGN KEY (`operator_id`) REFERENCES `operators` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
