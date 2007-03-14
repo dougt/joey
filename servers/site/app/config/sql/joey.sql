cfd6a7633e1935eda119b952aa7f5e0a7f78857a
wclouser@mozilla.com <wclouser@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-03-14 17:10:56 +0000

    Fixed db schema - this should resolve 373872
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@2629 4eb1ac78-321c-0410-a911-ec516a8615a5

--- servers/site/app/config/sql/joey.sql
+++ servers/site/app/config/sql/joey.sql
@@ -1,19 +1,5 @@
--- MySQL dump 10.10
+-- Mozilla Labs: Joey SQL Dump
 --
--- Host: localhost    Database: c_joey
--- ------------------------------------------------------
--- Server version	5.0.18
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `cake_sessions`
@@ -25,7 +11,58 @@ CREATE TABLE `cake_sessions` (
   `data` text,
   `expires` int(11) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `contentsource`
+--
+
+DROP TABLE IF EXISTS `contentsource`;
+CREATE TABLE `contentsource` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `upload_id` int(11) unsigned NOT NULL,
+  `source` text,
+  `contentsourcetype_id` int(11) unsigned NOT NULL default '0',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `upload_id` (`upload_id`),
+  KEY `contentsource_ibfk_2` (`contentsourcetype_id`),
+  CONSTRAINT `contentsource_ibfk_2` FOREIGN KEY (`contentsourcetype_id`) REFERENCES `contentsourcetype` (`id`),
+  CONSTRAINT `contentsource_ibfk_1` FOREIGN KEY (`upload_id`) REFERENCES `files` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `contentsourcetype`
+--
+
+DROP TABLE IF EXISTS `contentsourcetype`;
+CREATE TABLE `contentsourcetype` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL default '',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `upload_id` int(11) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL default '',
+  `size` int(11) unsigned NOT NULL default '0',
+  `type` varchar(255) NOT NULL default '',
+  `preview` varchar(255) NOT NULL default '',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `upload_id` (`upload_id`),
+  CONSTRAINT `files_ibfk_1` FOREIGN KEY (`upload_id`) REFERENCES `files` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `uploads`
@@ -34,22 +71,15 @@ CREATE TABLE `cake_sessions` (
 DROP TABLE IF EXISTS `uploads`;
 CREATE TABLE `uploads` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `owner` int(11) unsigned NOT NULL,
-  `name` varchar(255) NOT NULL default '',
-  `type` varchar(255) NOT NULL default '',
-  `uri` varchar(255) NOT NULL default '',
-  `uuid` varchar(255) NOT NULL default '',
+  `user_id` int(11) unsigned NOT NULL,
   `title` varchar(255) NOT NULL default '',
-  `size` int(11) unsigned NOT NULL default '0',
-  `content` longblob,
-  `filename` varchar(255) default NULL,
-  `thumbnailname` varchar(255) default NULL,
+  `referrer` varchar(255) NOT NULL default '',
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  `shared` enum('yes','no') NOT NULL default 'yes',
   PRIMARY KEY  (`id`),
-  KEY `owner` (`owner`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `uploads_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `users`
@@ -58,25 +88,16 @@ CREATE TABLE `uploads` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `username` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL default '',
   `password` varchar(255) NOT NULL default '',
   `email` varchar(255) NOT NULL default '',
+  `confirmationcode` varchar(255) default NULL,
+  `phone` varchar(255) NOT NULL default '',
+  `phoneoperatorid` varchar(255) NOT NULL default '',
+  `notes` text,
+  `disabled` int(1) NOT NULL default '0',
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  `ip` varchar(255) NOT NULL default '',
-  `level` varchar(255) NOT NULL default '',
-  `isbanned` enum('yes','no') NOT NULL default 'no',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
