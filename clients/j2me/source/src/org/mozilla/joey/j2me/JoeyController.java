d9c489b9fafe4c100c523e9cf1ef04040b7abfd4
michael.koch@enough.de <michael.koch@enough.de@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-04-18 13:02:49 +0000

    - use UploadsView.
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@3363 4eb1ac78-321c-0410-a911-ec516a8615a5

--- clients/j2me/source/src/org/mozilla/joey/j2me/JoeyController.java
+++ clients/j2me/source/src/org/mozilla/joey/j2me/JoeyController.java
@@ -2,6 +2,8 @@ package org.mozilla.joey.j2me;
 
 import de.enough.polish.util.Locale;
 
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
@@ -20,6 +22,7 @@ import de.enough.polish.ui.SnapshotScreen;
 //#endif
 
 import java.util.Hashtable;
+import java.util.Vector;
 
 public class JoeyController
 	implements CommandListener, ItemCommandListener, ResponseHandler
@@ -28,26 +31,40 @@ public class JoeyController
 	private static final int VIEW_MAINMENU = 2;
 	private static final int VIEW_SNAPSHOT = 3;
 	private static final int VIEW_UPLOADS = 4;
+	private static final int VIEW_UPLOADS_DELETE_ALERT = 5;
+	private static final int VIEW_WAIT = 6;
 	
 	private static final Command CMD_EXIT = new Command(Locale.get("command.exit"), Command.EXIT, 1);
 //	private static final Command CMD_SELECT = new Command(Locale.get("command.select"), Command.SCREEN, 1);
 	private static final Command CMD_SELECT = List.SELECT_COMMAND;
 	private static final Command CMD_BACK = new Command(Locale.get("command.back"), Command.BACK, 1);
 	private static final Command CMD_LOGIN = new Command(Locale.get("command.login"), Command.SCREEN, 1);
+	private static final Command CMD_DELETE = new Command(Locale.get("command.delete"), Command.SCREEN, 1);
+
+
+	private static final Command CMD_YES = new Command(Locale.get("command.yes"), Command.SCREEN, 1);
+	private static final Command CMD_NO = new Command(Locale.get("command.no"), Command.BACK, 1);
 
 	private int currentViewId;
 	private Displayable currentView;
 	private MIDlet midlet;
 	private Display display;
+	private Vector uploads;
 	private CommunicationController downloadThread;
 
 	public JoeyController(MIDlet midlet)
 	{
 		this.midlet = midlet;
 		this.display = Display.getDisplay(midlet);
+		this.uploads = new Vector();
 		this.downloadThread = new CommunicationController();
 		this.downloadThread.setResponseHandler(this);
 		this.downloadThread.start();
+
+		this.uploads.addElement(new Upload("Test 1", null, null));
+		this.uploads.addElement(new Upload("Test 2", null, null));
+		this.uploads.addElement(new Upload("Test 3", null, null));
+		this.uploads.addElement(new Upload("Test 4", null, null));
 	}
 	
 	public void startApp()
@@ -92,8 +109,10 @@ public class JoeyController
 		//#endif
 			
 		case VIEW_UPLOADS:
-			view = new UploadsView();
+			view = new UploadsView(this.uploads);
 			view.addCommand(CMD_BACK);
+			view.addCommand(CMD_SELECT);
+			view.addCommand(CMD_DELETE);
 			view.setCommandListener(this);
 			return view;
 
@@ -129,8 +148,10 @@ public class JoeyController
 			break;
 			
 		case VIEW_UPLOADS:
+		case VIEW_UPLOADS_DELETE_ALERT:
 			handled = processCommandUploads(command);
 			break;
+			
 
 		default:
 			//#debug error
@@ -159,13 +180,27 @@ public class JoeyController
 			showView(VIEW_MAINMENU);
 			return true;
 		}
-		
+		else if (command == CMD_DELETE) {
+			showUploadDeleteAlert();
+			return true;
+		}
+		else if (command == CMD_YES) {
+			// TODO: Show wait alert here while deletion is happening. 
+			//showWaitAlert();
+			this.downloadThread.delete(((List) this.currentView).getSelectedIndex());
+			showView(VIEW_UPLOADS);
+			return true;
+		}
+		else if (command == CMD_NO) {
+			showView(VIEW_UPLOADS);
+			return true;
+		}
+
 		return false;
 	}
 
 	private boolean processCommandMainMenu(Command command)
 	{
-		System.out.println("processCommandMainMenu 1");
 		if (command == CMD_SELECT) {
 			switch (((MainMenuView) this.currentView).getCurrentIndex()) {
 				case 0:
@@ -179,7 +214,6 @@ public class JoeyController
 
 			return true;
 		}
-		System.out.println("processCommandMainMenu 2");
 		
 		return false;
 	}
@@ -210,5 +244,29 @@ public class JoeyController
 	public void notifyResponse(Hashtable response)
 	{
 		// TODO: Do something.
+	}
+	
+	private void showUploadDeleteAlert()
+	{
+		//#style alert
+		Alert alert = new Alert( null, Locale.get("uploads.delete.msg"), null, AlertType.CONFIRMATION );
+		alert.setTimeout(Alert.FOREVER);
+		alert.addCommand(CMD_YES);
+		alert.addCommand(CMD_NO);
+		alert.setCommandListener(this);
+		this.display.setCurrent(alert);
+
+		this.currentViewId = VIEW_UPLOADS_DELETE_ALERT;
+	}
+	
+	private void showWaitAlert()
+	{
+		//#style waitAlert
+		Alert alert = new Alert( null, Locale.get("alert.wait.msg"), null, AlertType.INFO );
+		alert.setTimeout(Alert.FOREVER);
+		alert.setCommandListener(this);
+		this.display.setCurrent(alert);
+
+		this.currentViewId = VIEW_WAIT;
 	}
 }
