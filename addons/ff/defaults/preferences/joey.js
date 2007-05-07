c58e2c15ca13627b0007d1b6f383fbc784cdf1b2
mgalli@mgalli.com <mgalli@mgalli.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-05-07 22:17:06 +0000

    Ask for title option
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@3832 4eb1ac78-321c-0410-a911-ec516a8615a5

--- addons/ff/defaults/preferences/joey.js
+++ addons/ff/defaults/preferences/joey.js
@@ -1,4 +1,5 @@
 pref("joey.service_url","https://joey.labs.mozilla.com");
 pref("joey.enable_logging",true);
 pref("joey.firstRun",true);
+pref("joey.askForTitle",true);
 pref("joey.remember_username",false);
