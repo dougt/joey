b8e6150cd6c79bcfcee5e84dbf617d776c1ebc94
michael.koch@enough.de <michael.koch@enough.de@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-05-30 10:11:00 +0000

    - use a FramedForm for the uploads.
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@4173 4eb1ac78-321c-0410-a911-ec516a8615a5

--- clients/j2me/source/src/org/mozilla/joey/j2me/views/UploadsView.java
+++ clients/j2me/source/src/org/mozilla/joey/j2me/views/UploadsView.java
@@ -24,13 +24,19 @@
 
 package org.mozilla.joey.j2me.views;
 
+import de.enough.polish.ui.FramedForm;
+import de.enough.polish.ui.StyleSheet;
 import de.enough.polish.ui.UiAccess;
 import de.enough.polish.util.Locale;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.Vector;
 
-import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Choice;
+import javax.microedition.lcdui.ChoiceGroup;
+import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.ImageItem;
 import javax.microedition.lcdui.Item;
@@ -40,19 +46,81 @@ import org.mozilla.joey.j2me.JoeyController;
 import org.mozilla.joey.j2me.Upload;
 
 public class UploadsView
-	extends Form
+	extends FramedForm
 {
+	private ChoiceGroup commands;
+
 	public UploadsView(JoeyController controller, Vector uploads)
 	{
 		//#style uploadScreen
 		super(Locale.get("title.uploads"));
 
+		initCommandsArea(controller);
 		update(controller, uploads);
+	}
+
+	private void initCommandsArea(JoeyController controller)
+	{
+		//#style commandsarea
+		this.commands = new ChoiceGroup(null, Choice.EXCLUSIVE);
+		append(Graphics.BOTTOM, this.commands);
+
+		Image imgSelect = null;
+		Image imgDelete = null;
+		Image img3 = null;
+		Image img4 = null;
+		Image img5 = null;
+		Image img6 = null;
+		
+		try
+		{
+			imgSelect = Image.createImage("/icon_red.png");
+			imgDelete = Image.createImage("/icon_delete.png");
+			img3 = Image.createImage("/icon_green.png");
+			img4 = Image.createImage("/icon_blue.png");
+			img5 = Image.createImage("/icon_white.png");
+			img6 = Image.createImage("/icon_yellow.png");
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//#style commandsitem
+		this.commands.append(null, imgSelect);
+		this.commands.setDefaultCommand(JoeyController.CMD_SELECT);
+		this.commands.setItemCommandListener(controller);
+		
+		//#style commandsitem
+		this.commands.append(null, imgDelete);
+		this.commands.setDefaultCommand(JoeyController.CMD_DELETE);
+		this.commands.setItemCommandListener(controller);
+
+		//#style commandsitem
+		this.commands.append(null, img3);
+		this.commands.setDefaultCommand(JoeyController.CMD_SELECT);
+		this.commands.setItemCommandListener(controller);
+		
+		//#style commandsitem
+		this.commands.append(null, img4);
+		this.commands.setDefaultCommand(JoeyController.CMD_SELECT);
+		this.commands.setItemCommandListener(controller);
+		
+		//#style commandsitem
+		this.commands.append(null, img5);
+		this.commands.setDefaultCommand(JoeyController.CMD_SELECT);
+		this.commands.setItemCommandListener(controller);
+		
+		//#style commandsitem
+		this.commands.append(null, img6);
+		this.commands.setDefaultCommand(JoeyController.CMD_SELECT);
+		this.commands.setItemCommandListener(controller);
 	}
 
 	public void update(JoeyController controller, Vector uploads)
 	{
-		deleteAll();
+		this.container.clear();
 
 		for (int i = 0; i < uploads.size(); i++) {
 			Upload upload = (Upload) uploads.elementAt(i); 
@@ -84,5 +152,24 @@ public class UploadsView
 			UiAccess.setAttribute(item, JoeyController.ATTR_UPLOAD, upload);
 			append(item);
 		}
+	}
+
+	protected boolean handleKeyPressed(int keyCode, int gameAction)
+	{
+		if ((gameAction == LEFT && keyCode != Canvas.KEY_NUM4)
+			|| (gameAction == RIGHT && keyCode != Canvas.KEY_NUM6) 
+			|| (gameAction == FIRE && keyCode != Canvas.KEY_NUM5)) {
+			return UiAccess.handleKeyPressed( this.commands, keyCode, gameAction);
+		}
+
+		boolean handled = super.handleKeyPressed(keyCode, gameAction);
+
+		if (handled && this.currentlyActiveContainer == this.bottomFrame ) {
+			this.currentlyActiveContainer = this.container;
+			this.container.focus(0);
+			//#= this.bottomFrame.defocus(StyleSheet.commandsareaStyle);
+		}
+
+		return handled;
 	}
 }
