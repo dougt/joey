502212c1bfd9d85c45cdb79e361b3708d0fa3f65
dougt@meer.net <dougt@meer.net@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-06-06 06:24:52 +0000

    admin panel
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@4357 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ server/app/controllers/admin_controller.php
@@ -0,0 +1,64 @@
+<?php
+
+
+
+class AdminController extends AppController
+{
+
+  var $name = 'Admin';
+
+      var $components = array('Joey', 'Pagination', 'Session', 'Storage');
+
+//@todo review these
+    var $uses = array('Phone', 'Contentsource', 'Contentsourcetype', 'File', 'Upload','User');
+
+    var $helpers = array('Number','Time');
+
+    /**
+     * Set in beforeFilter().  Will hold the session user data.
+     */
+    var $_user;
+
+    /**
+     * You can thank https://trac.cakephp.org/ticket/1589 for not letting us put this
+     * in the constructor.  (Apparently that is not a valid scenario...)
+     */
+    function beforeFilter() {
+
+        parent::beforeFilter();
+
+        // Set the local user variable to the Session's User
+        $this->_user = $this->Session->read('User');
+
+
+        // disable query caching so devcp changes are visible immediately
+        foreach ($this->uses as $_model) {
+            $this->$_model->caching = false;
+        }
+    }
+
+    function index() {
+        $this->summary();
+    }
+
+
+    /**
+    * Admin Summary
+    */
+    function summary() {
+
+      if ($this->_user['administrator'] == 0)
+      {
+        $this->flash('Permission Denined.', '/uploads/index');
+      }
+
+  
+      $_summary["user_count"] = $this->User->findCount();
+      $_summary["upload_count"] = $this->Upload->findCOunt();
+
+
+      $this->set('summary', $_summary);
+
+    }
+}
+?>
