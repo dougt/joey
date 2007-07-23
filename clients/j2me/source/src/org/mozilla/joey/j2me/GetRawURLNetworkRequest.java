cf839de3d0d0abcc2c3268527edc10a1f9fd2969
dougt@meer.net <dougt@meer.net@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-07-23 17:38:23 +0000

    Raw support
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@5159 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ clients/j2me/source/src/org/mozilla/joey/j2me/GetRawURLNetworkRequest.java
@@ -0,0 +1,56 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Joey.
+ *
+ * The Initial Developer of the Original Code is
+ * Doug Turner.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.mozilla.joey.j2me;
+
+public class GetRawURLNetworkRequest
+    extends NetworkRequest
+{
+
+    /**
+     * Only valid after completion.
+     */
+    public String raw_url;
+
+    public GetRawURLNetworkRequest(String id)
+    {
+        this.requestURL = "/uploads/raw/" + id;
+        this.contenttype = "application/x-www-form-urlencoded";
+        this.postdata = "rest=1";
+    }
+
+    public void onStop() {
+
+		if (this.responseCode == 200) {
+            this.raw_url = this.data.toString();
+        }
+
+        if (this.handler != null) {
+            this.handler.notifyResponse(this);
+        }
+
+        this.notify();
+    }
+
+}
