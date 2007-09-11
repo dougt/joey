877d22353a8cb1261b1306359841f36154ec757c
michael.koch@enough.de <michael.koch@enough.de@4eb1ac78-321c-0410-a911-ec516a8615a5>
2007-09-11 15:52:22 +0000

    - updated preferences view
    git-svn-id: http://svn.mozilla.org/labs/joey/trunk@6721 4eb1ac78-321c-0410-a911-ec516a8615a5

--- clients/j2me/source/src/org/mozilla/joey/j2me/views/PreferencesView.java
+++ clients/j2me/source/src/org/mozilla/joey/j2me/views/PreferencesView.java
@@ -26,14 +26,74 @@ package org.mozilla.joey.j2me.views;
 
 import de.enough.polish.util.Locale;
 
+import javax.microedition.lcdui.Choice;
+import javax.microedition.lcdui.ChoiceGroup;
 import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.TextField;
 
 public class PreferencesView
 	extends Form
 {
+	private ChoiceGroup server;
+	private TextField updateInterval;
+	
 	public PreferencesView()
 	{
 		//#style inputScreen
 		super(Locale.get("form.preferences"));
+		
+		//#style radiobox
+		this.server = new ChoiceGroup(Locale.get("form.preferences.server"), Choice.EXCLUSIVE);
+		append(this.server);
+
+		//#style radioButton
+		this.server.append("joey.labs.mozilla.com", null);
+
+		//#style radioButton
+		this.server.append("dougt.joey-dev.labs.mozilla.com", null);
+
+		//#style radioButton
+		this.server.append("clouserw.joey-dev.labs.mozilla.com", null);
+
+		//#style input
+		this.updateInterval = new TextField(Locale.get("form.preferences.update_interval"), "", 10, TextField.DECIMAL);
+		append(this.updateInterval);
+	}
+
+	public String getServer()
+	{
+		return this.server.getString(this.server.getSelectedIndex());
+	}
+
+	public void setServer(String server)
+	{
+		// iterate over all entries and set the right one to selected.
+		for (int index = 0; index < this.server.size(); index++) {
+			if (this.server.getString(index).equals(server)) {
+				this.server.setSelectedIndex(index, true);
+				break;
+			}
+		}
+	}
+	
+	public long getUpdateInterval()
+	{
+		String tmp = this.updateInterval.getString();
+
+		if (tmp != null) {
+			try {
+				return Long.parseLong(tmp);
+			}
+			catch (NumberFormatException e) {
+				return -1;
+			}
+		}
+
+		return -1;
+	}
+
+	public void setUpdateInterval(long updateInterval)
+	{
+		this.updateInterval.setString(Long.toString(updateInterval));
 	}
 }
